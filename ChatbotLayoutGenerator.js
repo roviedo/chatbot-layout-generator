5ace15170b8391fdc2aaf32cc8fb5765e02e5c50
Raul Oviedo <antoniorah@gmail.com>
2020-12-12 15:43:12 -0500

    Initial commit of index and main javascript file for generator

---
+++ ChatbotLayoutGenerator.js
@@ -0,0 +1,20 @@
+class ChatbotLayoutGenerator {
+    constructor(props) {
+        console.log(props);
+    }
+}
+
+
+var chatbotLayoutGeneratorProps = {
+    header: {
+        image: 'base64string/url',
+        backgroundColor: 'blue'
+    },
+    openOnDefault: true,
+    openCloseColor: 'black',
+    openCloseIcons: {
+        open: 'base64string/url',
+        close: 'base64string/url'
+    },
+    openCloseShape: 'square'
+}
